ba501161758fdd6b5c52988df7b0845196889b26
Patricia Santana <psantana.student@hsfi.us>
2016-12-07 22:59:39 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+ $(document).ready(function() {
+     alert("hello");
+     $("h1").keydown(function(e) {
+         switch(e.which) {
+             case 32:
+             break;
+         }
+     
+       $('h1'), css('color', 'pink'); 
+     });
+ });
+ 
