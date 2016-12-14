52cd299c2a5c8f1abde8256553a8a753c7891062
Patricia Santana <psantana.student@hsfi.us>
2016-12-14 20:03:25 +0000

    added jquery

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
  $(document).ready(function() {
-     alert("hello");
-     $("h1").keydown(function(e) {
-         switch(e.which) {
-             case 32:
-             break;
+
+    $(document).keydown(function() {
+       $('body').keydown(function(e) {
+        if (e.which === 32) {
+         $('h1').css('background-color', 'pink');
+        }
+        else if (keycode === 69) {
+         while (i < 6) {
+          $("body").append("🌈")
+          i++
          }
-     
-       $('h1'), css('color', 'pink'); 
-     });
- });
+        };
+
+        if (e.which === 71) {
+
+         $("body").append("https://media.giphy.com/media/14g4L6kXIdGHU4/giphy.gif");
+        }
+       });
  
